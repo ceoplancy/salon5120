2b36db405369abb94dc0ee455ffeafd337c9cf80
psw <psw0962@naver.com>
2023-12-14 10:45:04 +0900

    working

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -39,6 +39,11 @@ export default class CustomDocument extends Document {
           <meta name="twitter:description" content="문화살롱 문화살롱5120 시작하는 예술가가 꿈을 키워갈 수 있는 기반을 마련하고 구민이 문화예술을 향유하는 일상 속의 장소" />
           <meta name="twitter:image" content="/image/seoimg.png"></meta>
 
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest"></link>
+
           <style
             dangerouslySetInnerHTML={{
               __html: `</style>
